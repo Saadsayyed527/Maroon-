077c8c0b40bfa7a0988f01ebac891edf9d0de61d
Samir Mulla <samirmulla710@gmail.com>
2025-01-05 15:37:58 +0530

    Component : NotFound is Completed

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <section className="flex justify-center">
+        <h2>404 | Page Not Found</h2>
+      </section>
+    </>
+  );
+};
+
+export default NotFound;
