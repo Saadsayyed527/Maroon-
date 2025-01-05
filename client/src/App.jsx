ee3f0f8177357dd32bd5dbca3d3ba96f17315e98
Samir Mulla <samirmulla710@gmail.com>
2025-01-05 15:58:05 +0530

    Component : App with Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+// import ServicesAccordion from "./components/ServicesAccordion";
+// import servicesData from "./data/data";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/AboutUs"
+import Services from "./pages/Services"
+import ContactUs from "./pages/ContactUs"
+import NotFound from "./components/NotFound";
+
+// import Heading from "./components/Heading";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-    </div>
+   <>
+   <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="/about" element={<About />} />
+   <Route path="/services" element={<Services />} />
+   <Route path="/contact" element={<ContactUs />} />
+   <Route path="*" element={<NotFound />} />
+   </Routes>
+   </>
   );
-}
+};
 
-export default App
+export default App;
