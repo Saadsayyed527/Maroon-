1e081f20e66ea6ed87a384d23219f6a4e31faa9b
Samir Mulla <samirmulla710@gmail.com>
2025-01-08 02:35:10 +0530

    Data : Added Client Data

---
+++ client/src/data/clientdata.js
@@ -0,0 +1,52 @@
+const clientData = [
+  {
+    id: 1,
+    image: "client1.png",
+    companyName: "My Mess (Our Mess Services Pvt Ltd)*",
+    services: [
+      "Logo & Branding,",
+      "Legal Documentation,",
+      "Web-Design,",
+      "Website Development,",
+      "App UI/UX",
+    ],
+    link: "https://projectA.com",
+  },
+  {
+    id: 2,
+    image: "client2.png",
+    companyName: "S G Construction*",
+    services: ["PPT Presentations,", "LinkedIn Setup"],
+    link: "https://projectB.com",
+  },
+  {
+    id: 3,
+    image: "client3.png",
+    companyName: "Jivan Aanand Saree Depot",
+    services: ["Rebranding logo,", "Google Maps"],
+    link: "https://projectC.com",
+  },
+  {
+    id: 4,
+    image: "client4.svg",
+    companyName: "The Art Of Living",
+    services: ["Posters,", "Whatsapp Marketing"],
+    link: "https://projectD.com",
+  },
+  {
+    id: 5,
+    image: "client5.png",
+    companyName: "MIT ADT University",
+    services: ["Posters,", "Whatsapp Marketing"],
+    link: "https://projectE.com",
+  },
+  {
+    id: 6,
+    image: "reserved-client.svg",
+    companyName: "This spot is reserved for You and Your Brand let's build your success story together. 🚀",
+    services: ["", ""],
+    link: "",
+  },
+];
+
+export default clientData;
