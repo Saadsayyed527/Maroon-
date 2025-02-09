2bcde026cece319dff307ca6a494ab9cb46918ae
Samir Mulla <samirmulla710@gmail.com>
2025-02-09 15:17:53 +0530

    Component : ServicesBackward is ready

---
+++ client/src/components/ServicesBackward.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BackwardIcon from "/backwardIcon.svg";
+
+function ServicesBackward() {
+  return (
+    <>
+    <div>
+        <img src={BackwardIcon} alt='Backward Icon' width={100} height={100} />
+    </div>
+    </>
+  )
+}
+
+export default ServicesBackward;
