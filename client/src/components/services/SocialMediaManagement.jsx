a3c2d443d526b0b31bb168cdeb285f3dbd716b97
Kaushal Singh <kaushalsingh1715@gmail.com>
2025-02-23 13:32:56 +0530

    Update SocialMediaManagement.jsx

--- client/src/components/services/SocialMediaManagement.jsx
+++ client/src/components/services/SocialMediaManagement.jsx
@@ -1,3 +1,7 @@
+// pages/services/DigitalMarketing.js
+// import React from "react";
+// import ServicesAccordion from "../ServicesAccordion";
+import ServicesHeading from "../ServicesHeading";
 
 import React, { useState } from "react";
 import accordionDownIcon from "/accordionDownIcon.svg";
@@ -735,212 +739,205 @@ const ServicesAccordion6 = () => {
 
 
 
-const ServicesAccordion7 = () => {
-  const [isOpen, setIsOpen] = useState(false);
+// const ServicesAccordion7 = () => {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
+//   const toggleAccordion = () => {
+//     setIsOpen(!isOpen);
+//   };
 
-  return (
-    <div
-      className={`font-outfit mb-8 ${
-        isOpen
-          ? "rounded-[16px] border border-black shadow-accordionBody"
-          : "rounded-[17px]"
-      }`}
-    >
-      {/* Accordion Header */}
-      <div
-        className={`flex items-center px-4 py-1 bg-[#D9D9D9] text-black cursor-pointer ${
-          isOpen ? "rounded-t-[15px]" : " rounded-[15px] shadow-accordionHead"
-        }`}
-        onClick={toggleAccordion}
-      >
-        {/* Icon on the left */}
-        <span className="mr-4">
-          <img src={instagramIcon} alt="Service Icon" />
-        </span>
-        {/* Heading and Paragraph */}
-        <div className="w-[600px]">
-          <h3 className="text-[1.5rem] font-outfit font-bold">
-            Instagram Management
-          </h3>
-          <p className="text-[1.2rem] font-normal w-[120%] leading-6">
-       <ul>
-        <li>
-        Amplify your brand's reach with innovative product marketing
-        </li>
-        <li>
-        strategies that connect with the right audience and boost sales.
+//   return (
+//     <div
+//       className={`font-outfit mb-8 ${
+//         isOpen
+//           ? "rounded-[16px] border border-black shadow-accordionBody"
+//           : "rounded-[17px]"
+//       }`}
+//     >
+//       {/* Accordion Header */}
+//       <div
+//         className={`flex items-center px-4 py-1 bg-[#D9D9D9] text-black cursor-pointer ${
+//           isOpen ? "rounded-t-[15px]" : " rounded-[15px] shadow-accordionHead"
+//         }`}
+//         onClick={toggleAccordion}
+//       >
+//         {/* Icon on the left */}
+//         <span className="mr-4">
+//           <img src={instagramIcon} alt="Service Icon" />
+//         </span>
+//         {/* Heading and Paragraph */}
+//         <div className="w-[600px]">
+//           <h3 className="text-[1.5rem] font-outfit font-bold">
+//             Instagram Management
+//           </h3>
+//           <p className="text-[1.2rem] font-normal w-[120%] leading-6">
+//             Amplify your brand's reach with innovative product marketing
+//             strategies that connect with the right audience and boost sales.
+//           </p>
+//         </div>
 
-        </li>
-       </ul>
-          </p>
-        </div>
+//         {/* Show Down Arrow only when accordion is closed */}
+//         {!isOpen && (
+//           <span className="mt-24 ml-12 max-w-none">
+//             <img
+//               src={accordionDownIcon}
+//               alt="Accordion Down Arrow Icon"
+//               className="w-7 ml-3" // Fixed size
+//             />
+//           </span>
+//         )}
+//         {/* Show Up Arrow only when accordion is open */}
+//         {isOpen && (
+//           <span className="mt-24 ml-12 max-w-none">
+//             <img
+//               // src={accordionUpIcon}
+//               // alt="Accordion Up Arrow Icon"
+//               className="w-7 ml-3" // Fixed size
+//             />
+//           </span>
+//         )}
+//       </div>
 
-        {/* Show Down Arrow only when accordion is closed */}
-        {!isOpen && (
-          <span className="mt-24 ml-12 max-w-none">
-            <img
-              src={accordionDownIcon}
-              alt="Accordion Down Arrow Icon"
-              className="w-7 ml-3" // Fixed size
-            />
-          </span>
-        )}
-        {/* Show Up Arrow only when accordion is open */}
-        {isOpen && (
-          <span className="mt-24 ml-12 max-w-none">
-            <img
-              // src={accordionUpIcon}
-              // alt="Accordion Up Arrow Icon"
-              className="w-7 ml-3" // Fixed size
-            />
-          </span>
-        )}
-      </div>
+//       {/* Accordion Body */}
+//       {isOpen && (
+//         <section>
+//           <div className="bg-gray-100 relative rounded-b-[15px] pt-4 pl-4">
+//             <h6 className="font-semibold text-lg leading-6">
+//               Content Creation:
+//             </h6>
+//             <ul className="mt-2 list-disc list list-inside font-normal text-lg leading-6">
+//               <li>Designing visually appealing posts, reels, and stories.</li>
+//               <li>Writing engaging captions with targeted hashtags.</li>
+//               <li>Creating carousel posts and IGTV videos.</li>
+//             </ul>
+//             <h6 className="mt-4 font-semibold text-lg leading-6">
+//               Content Creation:
+//             </h6>
+//             <ul className="mt-2 list-disc list-inside font-normal text-lg leading-6">
+//               <li>Designing visually appealing posts, reels, and stories.</li>
+//               <li>Writing engaging captions with targeted hashtags.</li>
+//               <li>Creating carousel posts and IGTV videos.</li>
+//             </ul>
 
-      {/* Accordion Body */}
-      {isOpen && (
-        <section>
-          <div className="bg-gray-100 relative rounded-b-[15px] pt-4 pl-4">
-            <h6 className="font-semibold text-lg leading-6">
-              Content Creation:
-            </h6>
-            <ul className="mt-2 list-disc list list-inside font-normal text-lg leading-6">
-              <li>Designing visually appealing posts, reels, and stories.</li>
-              <li>Writing engaging captions with targeted hashtags.</li>
-              <li>Creating carousel posts and IGTV videos.</li>
-            </ul>
-            <h6 className="mt-4 font-semibold text-lg leading-6">
-              Content Creation:
-            </h6>
-            <ul className="mt-2 list-disc list-inside font-normal text-lg leading-6">
-              <li>Designing visually appealing posts, reels, and stories.</li>
-              <li>Writing engaging captions with targeted hashtags.</li>
-              <li>Creating carousel posts and IGTV videos.</li>
-            </ul>
-
-            {/* Arrow Up Icon inside the Accordion Body */}
-            <span
-              className=" flex justify-end pr-1 pb-1 cursor-pointer"
-              onClick={toggleAccordion}
-            >
-              <img
-                src={accordionUpIcon}
-                alt="Accordion Up Arrow Icon"
-                className="w-7 block "
-              />
-            </span>
-          </div>
-        </section>
-      )}
-    </div>
-  );
-};
+//             {/* Arrow Up Icon inside the Accordion Body */}
+//             <span
+//               className=" flex justify-end pr-1 pb-1 cursor-pointer"
+//               onClick={toggleAccordion}
+//             >
+//               <img
+//                 src={accordionUpIcon}
+//                 alt="Accordion Up Arrow Icon"
+//                 className="w-7 block "
+//               />
+//             </span>
+//           </div>
+//         </section>
+//       )}
+//     </div>
+//   );
+// };
 
 
 
 
 
-const ServicesAccordion8 = () => {
-  const [isOpen, setIsOpen] = useState(false);
+// const ServicesAccordion8 = () => {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
+//   const toggleAccordion = () => {
+//     setIsOpen(!isOpen);
+//   };
 
-  return (
-    <div
-      className={`font-outfit mb-8 ${
-        isOpen
-          ? "rounded-[16px] border border-black shadow-accordionBody"
-          : "rounded-[17px]"
-      }`}
-    >
-      {/* Accordion Header */}
-      <div
-        className={`flex items-center px-4 py-1 bg-[#D9D9D9] text-black cursor-pointer ${
-          isOpen ? "rounded-t-[15px]" : " rounded-[15px] shadow-accordionHead"
-        }`}
-        onClick={toggleAccordion}
-      >
-        {/* Icon on the left */}
-        <span className="mr-4">
-          <img src={instagramIcon} alt="Service Icon" />
-        </span>
-        {/* Heading and Paragraph */}
-        <div className="w-[600px]">
-          <h3 className="text-[1.5rem] font-outfit font-bold">
-            Instagram Management
-          </h3>
-          <p className="text-[1.2rem] font-normal w-[120%] leading-6">
-            Amplify your brand's reach with innovative product marketing
-            strategies that connect with the right audience and boost sales.
-          </p>
-        </div>
+//   return (
+//     <div
+//       className={`font-outfit mb-8 ${
+//         isOpen
+//           ? "rounded-[16px] border border-black shadow-accordionBody"
+//           : "rounded-[17px]"
+//       }`}
+//     >
+//       {/* Accordion Header */}
+//       <div
+//         className={`flex items-center px-4 py-1 bg-[#D9D9D9] text-black cursor-pointer ${
+//           isOpen ? "rounded-t-[15px]" : " rounded-[15px] shadow-accordionHead"
+//         }`}
+//         onClick={toggleAccordion}
+//       >
+//         {/* Icon on the left */}
+//         <span className="mr-4">
+//           <img src={instagramIcon} alt="Service Icon" />
+//         </span>
+//         {/* Heading and Paragraph */}
+//         <div className="w-[600px]">
+//           <h3 className="text-[1.5rem] font-outfit font-bold">
+//             Instagram Management
+//           </h3>
+//           <p className="text-[1.2rem] font-normal w-[120%] leading-6">
+//             Amplify your brand's reach with innovative product marketing
+//             strategies that connect with the right audience and boost sales.
+//           </p>
+//         </div>
 
-        {/* Show Down Arrow only when accordion is closed */}
-        {!isOpen && (
-          <span className="mt-24 ml-12 max-w-none">
-            <img
-              src={accordionDownIcon}
-              alt="Accordion Down Arrow Icon"
-              className="w-7 ml-3" // Fixed size
-            />
-          </span>
-        )}
-        {/* Show Up Arrow only when accordion is open */}
-        {isOpen && (
-          <span className="mt-24 ml-12 max-w-none">
-            <img
-              // src={accordionUpIcon}
-              // alt="Accordion Up Arrow Icon"
-              className="w-7 ml-3" // Fixed size
-            />
-          </span>
-        )}
-      </div>
+//         {/* Show Down Arrow only when accordion is closed */}
+//         {!isOpen && (
+//           <span className="mt-24 ml-12 max-w-none">
+//             <img
+//               src={accordionDownIcon}
+//               alt="Accordion Down Arrow Icon"
+//               className="w-7 ml-3" // Fixed size
+//             />
+//           </span>
+//         )}
+//         {/* Show Up Arrow only when accordion is open */}
+//         {isOpen && (
+//           <span className="mt-24 ml-12 max-w-none">
+//             <img
+//               // src={accordionUpIcon}
+//               // alt="Accordion Up Arrow Icon"
+//               className="w-7 ml-3" // Fixed size
+//             />
+//           </span>
+//         )}
+//       </div>
 
-      {/* Accordion Body */}
-      {isOpen && (
-        <section>
-          <div className="bg-gray-100 relative rounded-b-[15px] pt-4 pl-4">
-            <h6 className="font-semibold text-lg leading-6">
-              Content Creation:
-            </h6>
-            <ul className="mt-2 list-disc list list-inside font-normal text-lg leading-6">
-              <li>Designing visually appealing posts, reels, and stories.</li>
-              <li>Writing engaging captions with targeted hashtags.</li>
-              <li>Creating carousel posts and IGTV videos.</li>
-            </ul>
-            <h6 className="mt-4 font-semibold text-lg leading-6">
-              Content Creation:
-            </h6>
-            <ul className="mt-2 list-disc list-inside font-normal text-lg leading-6">
-              <li>Designing visually appealing posts, reels, and stories.</li>
-              <li>Writing engaging captions with targeted hashtags.</li>
-              <li>Creating carousel posts and IGTV videos.</li>
-            </ul>
+//       {/* Accordion Body */}
+//       {isOpen && (
+//         <section>
+//           <div className="bg-gray-100 relative rounded-b-[15px] pt-4 pl-4">
+//             <h6 className="font-semibold text-lg leading-6">
+//               Content Creation:
+//             </h6>
+//             <ul className="mt-2 list-disc list list-inside font-normal text-lg leading-6">
+//               <li>Designing visually appealing posts, reels, and stories.</li>
+//               <li>Writing engaging captions with targeted hashtags.</li>
+//               <li>Creating carousel posts and IGTV videos.</li>
+//             </ul>
+//             <h6 className="mt-4 font-semibold text-lg leading-6">
+//               Content Creation:
+//             </h6>
+//             <ul className="mt-2 list-disc list-inside font-normal text-lg leading-6">
+//               <li>Designing visually appealing posts, reels, and stories.</li>
+//               <li>Writing engaging captions with targeted hashtags.</li>
+//               <li>Creating carousel posts and IGTV videos.</li>
+//             </ul>
 
-            {/* Arrow Up Icon inside the Accordion Body */}
-            <span
-              className=" flex justify-end pr-1 pb-1 cursor-pointer"
-              onClick={toggleAccordion}
-            >
-              <img
-                src={accordionUpIcon}
-                alt="Accordion Up Arrow Icon"
-                className="w-7 block "
-              />
-            </span>
-          </div>
-        </section>
-      )}
-    </div>
-  );
-};
+//             {/* Arrow Up Icon inside the Accordion Body */}
+//             <span
+//               className=" flex justify-end pr-1 pb-1 cursor-pointer"
+//               onClick={toggleAccordion}
+//             >
+//               <img
+//                 src={accordionUpIcon}
+//                 alt="Accordion Up Arrow Icon"
+//                 className="w-7 block "
+//               />
+//             </span>
+//           </div>
+//         </section>
+//       )}
+//     </div>
+//   );
+// };
 
 
 
